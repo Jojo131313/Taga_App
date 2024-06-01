1383bf3507f665ea922aefabd19e6398e8b61ac5
Josias <josephtakpo@gmail.com>
2024-06-01 02:16:01 +0000

    first commit

--- lib/screens/acceuil_screen.dart
+++ lib/screens/acceuil_screen.dart
@@ -1,14 +1,23 @@
+import 'package:boutoratakpa_cc_project/screens/car_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:boutoratakpa_cc_project/data.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Acceuil extends StatelessWidget {
+  List<Car> cars = getCarList();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(238, 159, 94, 0.98),
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
+        title: Text(
+          "Taga",
+          style: TextStyle(
+              fontSize: 35,
+              color: Color.fromARGB(255, 5, 34, 152),
+              fontWeight: FontWeight.bold),
+        ),
       ),
       drawer: Drawer(
         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
@@ -70,7 +79,109 @@ class Acceuil extends StatelessWidget {
           ],
         ),
       ),
-      // body: ,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Search...",
+                    hintStyle: TextStyle(fontSize: 16),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: BorderSide(
+                        width: 0,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 255, 255, 255),
+                    contentPadding: EdgeInsets.only(left: 30),
+                    suffixIcon: Padding(
+                      padding: EdgeInsets.only(right: 24, left: 16),
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                        size: 24,
+                      ),
+                    )),
+              ),
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 220, 215, 215),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "TOP DEALS",
+                            style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "view all",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 8, 29, 224),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 8,
+                              ),
+                              Icon(
+                                Icons.arrow_forward_ios,
+                                color: Color.fromARGB(255, 8, 29, 224),
+                                size: 17,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 280,
+                      child: ListView(
+                        physics: BouncingScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        children: buildDeals(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
+  }
+
+  List<Widget> buildDeals() {
+    List<Widget> list = [];
+    for (var i = 0; i < cars.length; i++) {
+      list.add(BuildCar(cars[i], i));
+    }
+    return list;
   }
 }
